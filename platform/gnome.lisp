1f392fc1c86cc55bd35ac3f5a53ef3e7ee2b7e5c
Iván Villagrasa Pérez <56278092+ivvil@users.noreply.github.com>
2025-07-04 18:32:26 +0200

    Added gnome platform

--- platform/gnome.lisp
+++ platform/gnome.lisp
@@ -8,8 +8,15 @@
 	   (search "GNOME" (uiop:getenv "XDG_CURRENT_DESKTOP") :test #'string-equal)))
 
 (defmethod set-wallpaper ((h gnome-platform) path &optional dark-path)
-  (let ))
+  (let ((uri (format nil "file://~a" (uiop:native-namestring (truename path)))))
+	(uiop:run-program `("gsettings" "set" "org.gnome.desktop.background" "picture-uri" ,uri)) ;; TODO: Use dconf directly
+	(when dark-path
+	  (let ((dark-uri (format nil "file://~a" (uiop:native-namestring (truename dark-path)))))
+		(uiop:run-program `("gsettings" "set" "org.gnome.desktop.background" "picture-uri" ,dark-uri))))))
 
-(defun set-dconf (key value &key (schema "org.gnome.desktop.background"))
-  "Set a dconf value through DBus"
-  (let* ))
+;; (defun set-dconf (key value &key (schema "org.gnome.desktop.background"))
+;;   "Set a dconf value through DBus"
+;;   (let ((bus dbus:session-server-addresses)
+;; 		(dconf-path (format nil "/~a/" (substitute #\/ #\. schema))))
+;; 	(dbus:with-introspected-object (writer bus "/ca/desrt/dconf/Writer/user" "ca.desrt.dconf")
+;; 	  (writer schema ))))
